b208256a52f0fdba44568a0404a13cbbf86c3d20
LeonardoPTC <leonardopcomitre@gmail.com>
2025-11-16 11:40:33 -0300

    feature: Finalizado botão pesquisar de cliente cadastrados

--- src/scripts/pages/funcionarios/index.js
+++ src/scripts/pages/funcionarios/index.js
@@ -1,8 +1,8 @@
-async function carregarClientes() {
-  const resposta = await fetch("http://localhost:5164/BlueMoon/Pessoas");
+async function carregarFuncionario() {
+  const resposta = await fetch("http://localhost:5164/BlueMoon/Usuarios");
   const clientes = await resposta.json();
 
-  const tbody = document.querySelector("#tabela-clientes tbody");
+  const tbody = document.querySelector("#tabela-funcionarios tbody");
   tbody.innerHTML = ""; // limpa a tabela
 
   clientes.forEach(c => {
@@ -10,11 +10,9 @@ async function carregarClientes() {
     let situacao = c.situacao;
 
     linha.innerHTML = `
-      <td>${c.codigo}</td>
+      <td>${c.codigoUsuario}</td>
       <td>${c.nome}</td>
-      <td>${c.telefone}</td>
-      <td>${c.cidade}</td>
-      <td>${c.endereco}</td>
+      <td>${c.cargo}</td>
       <td class="text-center">
           <button class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Visualizar Cliente" onclick="visualizarCliente('${c.id}')"><img src="/src/assets/View.png"></button>
           <button class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Editar Cliente" onclick="editarCliente('${c.id}')"><img src="/src/assets/Edit.png"></button>
@@ -25,7 +23,7 @@ async function carregarClientes() {
   });
 }
 
-carregarClientes();
+carregarFuncionario();
 
 document.addEventListener('DOMContentLoaded', function () {
     const checkSidebarLoaded = setInterval(function () {
@@ -46,45 +44,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-document.addEventListener('submit', async function (event) {
+/*document.addEventListener('submit', async function (event) {
   event.preventDefault();
 
   const codigo = document.getElementById('filterCodigo').value.trim();
   const nome = document.getElementById('filterNome').value;
-  const telefone = document.getElementById('filterTelefone').value;
-  const documento = document.getElementById('filterDocumento').value;
+  const marca = document.getElementById('filterMarca').value; //Alterar
 
   const dto = {
     codigo: codigo ? parseInt(codigo) : 0,
     nome: nome,
-    telefone: telefone,
-    documento: documento
+    marca: marca //Alterar
   };
 
   try {
-    const resposta = await fetch("http://localhost:5164/BlueMoon/Pessoas/Search", {
+    const resposta = await fetch("http://localhost:5164/BlueMoon/Produtos/Search", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(dto)
     });
 
     if (!resposta.ok) {
-      alert("Cliente não Encontrado!");
-      return;
+      alert("Produto não Encontrado!")
     }
 
-    const clientes = await resposta.json();
+    const produtos = await resposta.json();
 
-    const tbody = document.querySelector("#tabela-clientes tbody");
+    const tbody = document.querySelector("#tabela-produtos tbody");
     tbody.innerHTML = "";
-    clientes.forEach(c => {
+    produtos.forEach(p => {
       const linha = document.createElement("tr");
       linha.innerHTML = `
-      <td>${c.codigo}</td>
-      <td>${c.nome}</td>
-      <td>${c.telefone}</td>
-      <td>${c.cidade}</td>
-      <td>${c.endereco}</td>
+      <td>${p.codigo}</td>
+      <td>${p.nome}</td>
+      <td>${p.telefone}</td>
+      <td>${p.cidade}</td>
+      <td>${p.bairro + p.complemento}</td>
       <td class="text-center">
           <button class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Visualizar Cliente" onclick="visualizarCliente('${c.id}')"><img src="/src/assets/View.png"></button>
           <button class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Editar Cliente" onclick="editarCliente('${c.id}')"><img src="/src/assets/Edit.png"></button>
@@ -95,18 +90,17 @@ document.addEventListener('submit', async function (event) {
     })
 
   } catch (erro) {
-    alert("Erro ao aplicar filtro: " + erro.message);
+    alert("Erro ao aplicar filtro", erro);
   }
 });
 
 document.getElementById('limparFiltros').addEventListener('click', function () {
-  carregarClientes();
+  carregarProdutos();
   document.getElementById('filterCodigo').value = "";
   document.getElementById('filterNome').value = "";
-  document.getElementById('filterTelefone').value = "";
-  document.getElementById('filterDocumento').value = "";
+  document.getElementById('filterMarca').value = "";
 });
-
+*/
 function editarCliente(id) {
   window.location.href = `http://localhost:5500/src/pages/clientes/editcliente.html?id=${id}`;
 }
---
+++ src/scripts/pages/funcionarios/viewcliente.js
@@ -0,0 +1,126 @@
+window.addEventListener('load', async () => {
+    await includeHTML("header", "/src/include/header.html");
+    await includeHTML("footer", "/src/include/footer.html");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    if (id) carregarCliente(id);
+});
+
+async function carregarCliente(id) {
+
+    try {
+        const resposta = await fetch(`http://localhost:5164/BlueMoon/Pessoas/${id}`);
+
+        if (!resposta.ok) {
+            const erro = await resposta.text();
+            alert(erro);
+        }
+
+        cliente = await resposta.json();
+        console.log(cliente);
+        preencherCampos(cliente);
+    } catch (err) {
+        alert("Erro na conexão: " + err.message);
+    }
+}
+
+
+function preencherCampos(cliente) {
+
+    let cpf = cliente.documento || "";
+    let cnpj = cliente.documento || "";
+    let nome = cliente.nome || "";
+    let telefone = cliente.telefone || "";
+    let email = cliente.email || "";
+    let cep = cliente.cep || "";
+    let logradouro = cliente.logradouro || "";
+    let bairro = cliente.bairro || "";
+    let numero = cliente.numero || "";
+    let complemento = cliente.complemento || "";
+    let cidade = cliente.cidade || "";
+    let estado = cliente.estado || 0;
+    let inscricaoMunicipal = cliente.inscricaoMunicipal || "";
+    let inscricaoEstadual = cliente.inscricaoEstadual || "";
+    let tipo = cliente.tipo || 1;
+
+    for (let campo in cliente) {
+        if (cliente[campo] === "N/D") cliente[campo] = "";
+    }
+
+
+    const pessoaFisica = document.getElementById("pessoaFisica");
+    const pessoaJuridica = document.getElementById("pessoaJuridica");
+
+    if (tipo === 1) {
+        pessoaFisica.classList.remove("hidden");
+        pessoaJuridica.classList.add("hidden");
+        document.getElementById("ClienteFisico").checked = true;
+
+        document.getElementById("inputCPF").value = cpf;
+        document.getElementById("inputNomeFisico").value = nome;
+        document.getElementById("inputTelefoneFisico").value = telefone;
+        document.getElementById("inputEmailFisico").value = email;
+        document.getElementById("inputCEPFisico").value = cep;
+        document.getElementById("inputLogradouroFisico").value = logradouro;
+        document.getElementById("inputBairroFisico").value = bairro;
+        document.getElementById("inputNumeroFisico").value = numero;
+        document.getElementById("inputComplementoFisico").value = complemento;
+        document.getElementById("inputCidadeFisico").value = cidade;
+        const selectEstadoFisico = document.getElementById("estadoFisico");
+        if (estado && estado !== 0) {
+            selectEstadoFisico.value = estado;
+        } else {
+            selectEstadoFisico.value = 0;
+            selectEstadoFisico.options[0].text = "Estado não definido";
+        }
+    } else {
+        pessoaFisica.classList.add("hidden");
+        pessoaJuridica.classList.remove("hidden");
+        document.getElementById("ClienteJuridico").checked = true;
+        document.getElementById("inputCNPJ").value = cnpj;
+        document.getElementById("inputInscricaoMunicipal").value = inscricaoMunicipal;
+        document.getElementById("inputInscricaoEstadual").value = inscricaoEstadual;
+        document.getElementById("inputNomeJuridico").value = nome;
+        document.getElementById("inputTelefoneJuridico").value = telefone;
+        document.getElementById("inputEmailJuridico").value = email;
+        document.getElementById("inputCEPJuridico").value = cep;
+        document.getElementById("inputLogradouroJuridico").value = logradouro;
+        document.getElementById("inputBairroJuridico").value = bairro;
+        document.getElementById("inputNumeroJuridico").value = numero;
+        document.getElementById("inputComplementoJuridico").value = complemento;
+        document.getElementById("inputCidadeJuridico").value = cidade;
+        const selectEstadoJuridico = document.getElementById("estadoJuridico");
+        if (estado && estado !== 0) {
+            selectEstadoJuridico.value = estado;
+        } else {
+            selectEstadoJuridico.value = 0;
+            selectEstadoJuridico.options[0].text = "Estado não definido";
+        }
+    }
+
+    const tipoPessoa = document.getElementsByName('tipo');
+    const formularioClienteFisico = document.getElementById('pessoaFisica')
+    const formularioClienteJuridico = document.getElementById('pessoaJuridica')
+
+    tipoPessoa.forEach(radio => {
+        radio.addEventListener('change', () => {
+            if (document.getElementById("ClienteFisico").checked) {
+                formularioClienteFisico.classList.remove('hidden');
+                formularioClienteJuridico.classList.add('hidden');
+            } else if (document.getElementById("ClienteJuridico").checked) {
+                formularioClienteJuridico.classList.remove('hidden');
+                formularioClienteFisico.classList.add('hidden');
+            }
+        });
+    });
+
+    const inputs = document.querySelectorAll('#formCliente input, #formCliente select');
+    inputs.forEach(input => input.setAttribute('disabled', true));
+
+    const selects = document.querySelectorAll('#formCliente select');
+    selects.forEach(select => {
+        select.addEventListener('mousedown', e => e.preventDefault());
+        select.addEventListener('keydown', e => e.preventDefault());
+    });
+}
