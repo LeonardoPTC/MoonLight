1a86887c102c445198ed6ee3d94ccca0144937ef
LeonardoPTC <leonardopcomitre@gmail.com>
2025-11-16 19:50:26 -0300

    feature: Adição de cadastro de funcionário a partir de um registro pré-existente
    - Criada a função addfuncionario.html para cadastro de funcionários, com campos para cargo, salário, data de admissão e dados de acesso.
    
    - Implementado o arquivo addfuncionario.js para gerenciar o envio do formulário, a conversão de dados e a interação com a API para o cadastro de funcionários.
    
    - Desenvolvido o arquivo atualizacaofuncionario.html para atualização dos dados dos funcionários, incluindo informações pessoais e de contato.
    
    - Adicionado o arquivo atualizacaofuncionario.js para gerenciar o carregamento de dados de funcionários existentes e o tratamento de atualizações via API.
    
    - Aprimorado o arquivo consulta.js para oferecer suporte à funcionalidade de busca de clientes com filtros e seleção de clientes para atualizações.
    
    - Introduzidos estilos CSS para formulários e tabelas para melhorar a consistência e a responsividade da interface do usuário.

---
+++ src/scripts/pages/funcionarios/addfuncionario.js
@@ -0,0 +1,100 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    function converterDataParaBack(data) {
+        if (!data) return "";
+
+        const partes = data.split("-");
+        return partes[2] + partes[1] + partes[0];
+    }
+
+    const pessoaId = localStorage.getItem("pessoaId");
+
+    const btnVoltar = document.getElementById("btnVoltar");
+
+    btnVoltar.addEventListener("click", () => {
+        localStorage.setItem("pessoaId", pessoaId);
+        window.location.href = `/src/pages/funcionarios/atualizacaofuncionario.html?id=${pessoaId}`
+    });
+
+    if (!pessoaId) {
+        console.warn("Nenhuma pessoa foi selecionada antes de entrar nesta tela.");
+    }
+
+    const inputSenha = document.getElementById("senha");
+    const toggleSenha = document.getElementById("toggleSenha");
+
+    if (inputSenha && toggleSenha) {
+        toggleSenha.addEventListener("click", function () {
+            if (inputSenha.type === "password") {
+                inputSenha.type = "text";
+                this.textContent = "🙈";
+            } else {
+                inputSenha.type = "password";
+                this.textContent = "👁️";
+            }
+        });
+    }
+
+    const form = document.getElementById("formFuncionario");
+
+    if (!form) {
+        console.error("Formulário de funcionário não encontrado.");
+        return;
+    }
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+
+        const Login = document.getElementById("inputEmail").value;
+        const Senha = document.getElementById("senha").value;
+        const Cargo = document.getElementById("cargo").value;
+        const Salario = document.getElementById("inputSalario").value;
+        const Admissao = document.getElementById("inputAdmissao").value;
+        const AdmissaoConvertida = converterDataParaBack(Admissao);
+
+        const HorarioInicioCargaHoraria =
+            document.querySelector("input[name='HorarioInicioCargaHoraria']").value;
+
+        const HorarioFimCargaHoraria =
+            document.querySelector("input[name='HorarioFimCargaHoraria']").value;
+
+        const dto = {
+            login: Login,
+            senha: Senha,
+            cargo: Number(Cargo),
+            salario: Number(Salario),
+            admissao: AdmissaoConvertida,
+            horarioInicioCargaHoraria: HorarioInicioCargaHoraria.toString(),
+            horarioFimCargaHoraria: HorarioFimCargaHoraria.toString(),
+            idPessoa: pessoaId
+        };
+
+        console.log("DTO enviado:", dto);
+
+        try {
+            const resposta = await fetch("http://localhost:5164/BlueMoon/Usuarios", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dto)
+            });
+
+            const body = await resposta.text();
+
+            if (!resposta.ok) {
+                alert("Erro ao cadastrar funcionário: " + body);
+                return;
+            }
+
+            alert("Funcionário cadastrado com sucesso!");
+            window.location.href = "/src/pages/funcionarios/index.html";
+
+        } catch (erro) {
+            console.error(erro);
+            alert("Erro ao conectar com servidor.");
+        }
+    });
+
+});
